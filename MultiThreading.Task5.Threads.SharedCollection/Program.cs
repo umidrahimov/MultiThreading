058cf5b14422926dd7ad7aa5d0e43a1396aa6a57
Umid Rahimov <umid.ragimov@gmail.com>
2024-10-07 23:26:36 +0300

    Task 5 is completed

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,21 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        static List<String> collection;
+        static object _lock = new object();
+        static AutoResetEvent newItemAddedEvent = new AutoResetEvent(false);
+        static AutoResetEvent newItemPrintedEvent = new AutoResetEvent(true);
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -18,9 +28,34 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine();
 
             // feel free to add your code
-            
+            int collectionSize = 10;
+            collection = new List<string>(collectionSize);
+            Task writerTask = Task.Run(() => WriteToCollection(collectionSize));
+            Task readerTask = Task.Run(() => PrintCollection(10));
+
+            Task.WaitAll(writerTask, readerTask);
 
             Console.ReadLine();
+        }
+
+        private static void WriteToCollection(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                newItemPrintedEvent.WaitOne();
+                collection.Add($"Element {i}");
+                System.Console.WriteLine($"Writer task #{Thread.CurrentThread.ManagedThreadId} added element {i}.");
+                newItemAddedEvent.Set();
+            }
+        }
+        private static void PrintCollection(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                newItemAddedEvent.WaitOne();
+                System.Console.WriteLine($"Reader task #{Thread.CurrentThread.ManagedThreadId} printed collection itme {i}: {collection[i]}\n");
+                newItemPrintedEvent.Set();
+            }
         }
     }
 }
